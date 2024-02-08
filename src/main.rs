be3f5fea49ee94d93ce67adae48ae1a8435b8514
Jake Burton <jakeburton1997@gmail.com>
2024-02-08 17:42:06 +0000

    Guessing Game pt1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    	println!("Guess the number!");
+	println!("Please input your guess.");
+
+	let mut guess = String::new(); //mut means the variable is mutable
+	io::stdin()
+		.read_line(&mut guess) //& denotes the variable is used by reference
+		.expect("Failed to read line");
+	println!("You guessed: {guess}");
 }
